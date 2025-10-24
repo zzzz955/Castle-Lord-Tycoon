7e1d54eea7187cb0287fe404507d83cdc6e732a6
Sang <zzzzz955@gmail.com>
2025-10-24 20:47:54 +0900

    feat#2: 대장간 데이터 검수

--- CastleLordTycoon.Server/Domain/Forge/ForgeOptionChangeRule.cs
+++ CastleLordTycoon.Server/Domain/Forge/ForgeOptionChangeRule.cs
@@ -1,26 +1,17 @@
-using CsvHelper.Configuration.Attributes;
+﻿using CsvHelper.Configuration.Attributes;
 
 namespace CastleLordTycoon.Server.Domain.Forge;
 
 public sealed class ForgeOptionChangeRule
 {
-    [Name("grade_min")]
-    public string GradeMin { get; init; } = string.Empty;
-
-    [Name("grade_max")]
-    public string GradeMax { get; init; } = string.Empty;
+    [Name("grade")]
+    public string Grade { get; init; } = string.Empty;
 
     [Name("option_change_item_id")]
     public string OptionChangeItemId { get; init; } = string.Empty;
 
-    [Name("item_cost")]
-    public int ItemCost { get; init; }
-
-    [Name("gold_cost")]
-    public int GoldCost { get; init; }
-
-    [Name("reroll_stat_count")]
-    public int RerollStatCount { get; init; }
+    [Name("runs_required_count")]
+    public int RunsRequiredCount { get; init; }
 
     [Name("notes_ko")]
     public string NotesKo { get; init; } = string.Empty;
--- CastleLordTycoon.Server/Domain/Forge/ForgeSynthesisRule.cs
+++ CastleLordTycoon.Server/Domain/Forge/ForgeSynthesisRule.cs
@@ -1,4 +1,4 @@
-using CsvHelper.Configuration.Attributes;
+﻿using CsvHelper.Configuration.Attributes;
 
 namespace CastleLordTycoon.Server.Domain.Forge;
 
@@ -13,20 +13,11 @@ public sealed class ForgeSynthesisRule
     [Name("input_required_count")]
     public int InputRequiredCount { get; init; }
 
-    [Name("catalyst_item_id")]
-    public string CatalystItemId { get; init; } = string.Empty;
-
-    [Name("catalyst_amount")]
-    public int CatalystAmount { get; init; }
-
     [Name("gold_cost")]
     public int GoldCost { get; init; }
 
     [Name("success_rate_bps")]
     public int SuccessRateBasisPoints { get; init; }
-
-    [Name("protect_on_fail")]
-    public bool ProtectOnFail { get; init; }
 
     [Name("notes_ko")]
     public string NotesKo { get; init; } = string.Empty;
